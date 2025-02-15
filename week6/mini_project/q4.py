397cb0f29794608be97c51fa2195391e305f6571
scotty <rroge036@gmail.com>
2025-02-15 10:28:09 -0700

    objective 2 done and start objective 3

--- week6/mini_project/q4.py
+++ week6/mini_project/q4.py
@@ -5,6 +5,7 @@ matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.metrics import r2_score
+from sklearn.linear_model import LinearRegression
 
 # read 2016.csv
 df = pd.read_csv('2016.csv')
@@ -51,9 +52,8 @@ df = df.sort_values(by='sort_col', ascending=False).drop('sort_col', axis=1)
 
 # plots
 fig1, axes1 = plt.subplots(2,1,figsize=(15,10))
+# squish axes1 into 1d array
 axes1 = axes1.flatten()
-#fig1.suptitle('Happiness Score by Region')
-#sns.barplot(y=countries_by_region.index,x=countries_by_region.values,ax=axes1[0],hue=countries_by_region.index)
 sns.barplot(y=df.region.value_counts().index,x=df.region.value_counts().values,ax=axes1[0],hue=df.region.value_counts().index)
 axes1[0].set_ylabel('Region')
 axes1[0].set_xlabel('Number of Countries')
@@ -63,12 +63,11 @@ axes1[1].set_ylabel('Region')
 axes1[1].set_xlabel('Happiness Score')
 axes1[1].set_title('Happiness Score per Region')
 plt.tight_layout()
-#plt.show()
-#print(df.columns)
+plt.show()
+
 happiness_score_comparison= ['economy_gdp_per_capita', 'family', 'health_life_expectancy', 'freedom','trust_government_corruption', 'generosity', 'dystopia_residual']
 
 def calc_r_sqared(reg,x,y,col):
-    #print(f'r sqared for {reg}: {col}')
     x = x.to_numpy()
     y = y.to_numpy()
     coefficents = np.polyfit(x,y,1)
@@ -100,19 +99,18 @@ df_global['comparison_hs'] = [i.split('_',1)[1].strip('_vs_hs') for i in df_glob
 df_global.reset_index(inplace=True,drop=True)
 
 # check for correlations between happiness score and other numeric values by region
-comp_dict_regional={}
+comp_dict_regional_points={}
 for col in happiness_score_comparison:
     for reg in df.region.unique():
         x = df[df.region==reg].loc[:,col]
         y = df[df.region==reg].loc[:,'happiness_score']
         r2, npoints = calc_r_sqared(reg,x,y,col)
         key = "_".join([reg,col,'vs_hs'])
-        comp_dict_regional[key]=(r2,npoints)
+        comp_dict_regional_points[key]=(r2,npoints)
 
-# only can look at regions with enough data(n>=10)
-# note this cuts our observations in half 70 => 35
+# take all observations reguardless of number of points
+comp_dict_regional = {i:j[0] for i,j in comp_dict_regional_points.items()}
 
-comp_dict_regional = {i:j[0] for i,j in comp_dict_regional.items() if j[1] >=10}
 df_regional = pd.DataFrame.from_dict(comp_dict_regional, orient='index',columns =['r2_value'])
 df_regional.index.name ='comparison'
 df_regional.sort_values('r2_value',ascending=False,inplace=True)
@@ -122,43 +120,161 @@ df_regional.reset_index(inplace=True,drop=True)
 
 #combine regional and global results for r squared comparison for happiness score
 df_comp = pd.concat([df_global,df_regional])
+df_comp.replace('ealth_life_expectancy', 'health_life_expectancy', inplace=True)
 df_comp.sort_values('r2_value', ascending=False, inplace=True)
 df_comp.reset_index(drop=True, inplace=True)
 
-#only take top 10
-df_comp = df_comp.iloc[:10,:]
-print(df_comp.head())
 # Create the bar chart
 plt.figure(figsize=(12, 6))  # Adjust figure size for better readability
-sns.barplot(x='region', y='r2_value', hue='comparison_hs', data=df_comp, palette='Set3')
+sns.barplot(x='region', y='r2_value', hue='comparison_hs', data=df_comp,) #palette='Set3')
 
 plt.ylabel("R-squared Value")
 plt.xlabel("Region - Comparison")
+plt.xticks(rotation=45)
 plt.title("R-squared Values for Happiness Factor Comparisons")
 plt.tight_layout() # Adjust layout to prevent labels from overlapping
 plt.show()
-'''
-for col in happiness_score_comparison:
-    plt.figure()
-    sns.scatterplot(data=df,x=col,y='happiness_score')
-    plt.tight_layout()
-    plt.show()
-'''
+
+# now only looking at regions with enough data(n>=10)
+# note this cuts our observations in half 70 => 35
+comp_dict_regional = {i:j[0] for i,j in comp_dict_regional_points.items() if j[1] >=10}
+df_regional = pd.DataFrame.from_dict(comp_dict_regional, orient='index',columns =['r2_value'])
+df_regional.index.name ='comparison'
+df_regional.sort_values('r2_value',ascending=False,inplace=True)
+df_regional['region'] = [i.split('_')[0] for i in df_regional.index.values.tolist()]
+df_regional['comparison_hs'] = [i.split('_',1)[1].strip('_vs_hs') for i in df_regional.index.values.tolist()]
+df_regional.reset_index(inplace=True,drop=True)
+
+#combine regional and global results for r squared comparison for happiness score
+df_comp = pd.concat([df_global,df_regional])
+df_comp.replace('ealth_life_expectancy', 'health_life_expectancy', inplace=True)
+df_comp.sort_values('r2_value', ascending=False, inplace=True)
+df_comp.reset_index(drop=True, inplace=True)
+
+# Create the bar chart
+plt.figure(figsize=(12, 6))  # Adjust figure size for better readability
+sns.barplot(x='region', y='r2_value', hue='comparison_hs', data=df_comp,) #palette='Set3')
+plt.ylabel("R-squared Value")
+plt.xlabel("Region - Comparison")
+plt.xticks(rotation=45)
+plt.title("R-squared Values for Happiness Factor Comparisons(Regions with n>=10)")
+plt.tight_layout() # Adjust layout to prevent labels from overlapping
+plt.show()
+
 '''
 objective 2: To what extent does economic development (GDP per capita) influence
 happiness levels globally(1), and how does this relationship interact with other factors 
-like health and social support(2)? Are there diminishing returns to economic growth in 
+like health(2)? Are there diminishing returns to economic growth in 
 terms of happiness(3)?
 - (1) gdp vs happiness score
 - (2) gdp vs family and gdp vs health life expectancy
 - (3) look at plot from (1) does happiness plateau at a high enough gdp?
 '''
+# (1) gdp vs happiness score
+df_global.replace('ealth_life_expectancy', 'health_life_expectancy', inplace=True)
+# plots
+fig2, axes2 = plt.subplots(2,1,figsize=(15,10))
+# squish axes2 into 1d array
+axes2 = axes2.flatten()
+sns.barplot(x=df_global.r2_value,y=df_global.region,ax=axes2[0],hue=df_global.comparison_hs)
+axes2[0].set_ylabel('Globally')
+axes2[0].set_xlabel('R-squared Value')
+axes2[0].set_title('Metric Influence on Happiness Score Globally')
+sns.scatterplot(data=df, x='economy_gdp_per_capita', y='happiness_score',ax=axes2[1],hue='region')
+axes2[1].set_ylabel('Happiness Score')
+axes2[1].set_xlabel('GDP per Capita')
+axes2[1].set_title('Happiness Score vs GDP per Capita')
+plt.tight_layout()
+plt.show()
+
+#(2) gdp vs family and gdp vs health life expectancy
+# Calculate the correlation matrix for GDP and other numeric columns
+correlation_matrix = df[['economy_gdp_per_capita', 'happiness_score', 'health_life_expectancy', 'family', 'freedom', 'generosity', 'trust_government_corruption']].corr()
+plt.figure(figsize=(8, 6))
+heatmap = sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', vmin=-1, vmax=1)
+tick_labels = ['GDP', 'Happiness', 'Health', 'Family', 'Freedom', 'Generosity', 'Trust']
+heatmap.set_xticklabels(tick_labels, rotation=45)
+heatmap.set_yticklabels(tick_labels, rotation=0)
+plt.title('Correlation Heatmap: GDP and Numeric Metrics')
+plt.tight_layout()
+plt.show()
+
+# (3) look at plot from (1) does happiness plateau at a high enough gdp?
+fig3, axes3 = plt.subplots(1,2,figsize=(12, 6), sharey=True)  # Adjust figure size for better readability
+axes3 = axes3.flatten()
+# Plot GDP per Capita < 1.45 vs. Happiness Score
+sns.scatterplot(x='economy_gdp_per_capita', y='happiness_score', data=df.query('economy_gdp_per_capita < 1.45'),ax=axes3[0],alpha=0.5) 
+sns.regplot(x='economy_gdp_per_capita', y='happiness_score', data=df.query('economy_gdp_per_capita < 1.45'),ax=axes3[0], scatter=False, color='red')
+X1 = df.query('economy_gdp_per_capita < 1.45').economy_gdp_per_capita.values.reshape(-1,1)
+y1 = df.query('economy_gdp_per_capita < 1.45').happiness_score.values
+model_1 = LinearRegression().fit(X1, y1)
+r_squared_1 = model_1.score(X1, y1)
+slope_1 = model_1.coef_[0]
+intercept_1 = model_1.intercept_
+
+# Display R-squared value and line of best fiton the plot
+axes3[0].text(0.05, 0.95, f'R² = {r_squared_1:.2f}\n y={slope_1:.2f}x + {intercept_1:.2f}', transform=axes3[0].transAxes, fontsize=12, verticalalignment='top')
+axes3[0].set_ylabel("Happiness Score")
+axes3[0].set_xlabel("GDP per Capita")
+axes3[0].set_title("GDP per Capita <1.45")
+
+# Plot GDP per Capita >= 1.45 vs. Happiness Score
+sns.scatterplot(x='economy_gdp_per_capita', y='happiness_score', data=df.query('economy_gdp_per_capita >= 1.45'),ax=axes3[1],alpha=0.5) 
+sns.regplot(x='economy_gdp_per_capita', y='happiness_score', data=df.query('economy_gdp_per_capita >= 1.45'),ax=axes3[1], scatter=False, color='red')
+X2 = df.query('economy_gdp_per_capita >= 1.45').economy_gdp_per_capita.values.reshape(-1,1)
+y2 = df.query('economy_gdp_per_capita >= 1.45').happiness_score.values
+model_2 = LinearRegression().fit(X2, y2)
+r_squared_2 = model_2.score(X2, y2)
+slope_2 = model_2.coef_[0]
+intercept_2 = model_2.intercept_
+# Display R-squared value on the plot
+axes3[1].text(0.05, 0.95, f'R² = {r_squared_2:.2f}\n y={slope_2:.2f}x + {intercept_2:.2f}', transform=axes3[1].transAxes, fontsize=12,verticalalignment='top')
+#axes3[1].set_ylabel("Happiness Score")
+axes3[1].set_xlabel("GDP per Capita")
+axes3[1].set_title("GDP per Capita >=1.45")
+plt.tight_layout() # Adjust layout to prevent labels from overlapping
+plt.show()
 
 '''
 objective 3: How do trust in government and perceived freedom influence happiness levels(1), 
-and are these factors more important in certain regions or cultural contexts(2)? Is there a 
-link between corruption perception and happiness(3)?
+and are these factors more important in certain regions(2)? Is there a 
+link between trust and happiness(3)?
 - (1) trust in govt vs. happiness score and freedom vs. happiness (globally)
 - (2) trust in govt vs. happiness score and freedom vs. happiness (region)
 - (3) trust in govt corruption vs. happiness (top 5 and bottom 5)
 '''
+# (1) trust in govt vs. happiness score and freedom vs. happiness (globally)
+# plots for comparison btwn trust in govt and freedom vs happiness score
+fig4, axes4 = plt.subplots(1,2,figsize=(12, 6), sharey=True)  # Adjust figure size for better readability
+axes4 = axes4.flatten()
+# trust vs. Happiness Score
+sns.scatterplot(x='trust_government_corruption', y='happiness_score', data=df,ax=axes4[0],alpha=0.5) 
+sns.regplot(x='trust_government_corruption', y='happiness_score', data=df,ax=axes4[0], scatter=False, color='red')
+X1 = df.trust_government_corruption.values.reshape(-1,1)
+y1 = df.happiness_score.values
+model_1 = LinearRegression().fit(X1, y1)
+r_squared_1 = model_1.score(X1, y1)
+slope_1 = model_1.coef_[0]
+intercept_1 = model_1.intercept_
+
+# Display R-squared value and line of best fiton the plot
+axes4[0].text(0.05, 0.95, f'R² = {r_squared_1:.2f}\n y={slope_1:.2f}x + {intercept_1:.2f}', transform=axes4[0].transAxes, fontsize=12, verticalalignment='top')
+axes4[0].set_ylabel("Happiness Score")
+axes4[0].set_xlabel("Trust in Government")
+axes4[0].set_title("How Trust in Government Influences Happiness Score")
+
+# Plot freedom vs. Happiness Score
+sns.scatterplot(x='freedom', y='happiness_score', data=df,ax=axes4[1],alpha=0.5) 
+sns.regplot(x='freedom', y='happiness_score', data=df,ax=axes4[1], scatter=False, color='red')
+X2 = df.freedom.values.reshape(-1,1)
+y2 = df.happiness_score.values
+model_2 = LinearRegression().fit(X2, y2)
+r_squared_2 = model_2.score(X2, y2)
+slope_2 = model_2.coef_[0]
+intercept_2 = model_2.intercept_
+# Display R-squared value on the plot
+axes4[1].text(0.05, 0.95, f'R² = {r_squared_2:.2f}\n y={slope_2:.2f}x + {intercept_2:.2f}', transform=axes4[1].transAxes, fontsize=12,verticalalignment='top')
+axes4[1].set_xlabel("Freedom")
+axes4[1].set_title("How Freedom Influences Happiness Score")
+plt.tight_layout() # Adjust layout to prevent labels from overlapping
+plt.show()
